1588bcf85a1858c52ec83f00ecfa30d7974e1652
atharwa-pandey <76654647+atharwa-pandey@users.noreply.github.com>
2022-04-01 22:01:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal();
+sr.reveal('.headingLander' , {
+duration: 2000,
+origin:'bottom',
+distance: '50px'
+});
+window.sr = ScrollReveal();
+sr.reveal('.leftEntrance' , {
+duration: 2000,
+origin:'left',
+distance: '50px'
+});
